47ab3d6100d35afc679daeb3d151fabaee154d68
Grant Saggars <mumbo1002@gmail.com>
2025-08-08 17:46:42 -0500

    Create tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// frontend/tailwind.config.js
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}", // adjust if your source files are in src
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
