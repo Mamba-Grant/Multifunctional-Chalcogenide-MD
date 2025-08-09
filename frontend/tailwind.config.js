4d7db09a6800ed598bdd705c4aa0abf1779e191b
Grant Saggars <mumbo1002@gmail.com>
2025-08-08 19:06:53 -0500

    Attempt to fix css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}", // adjust if your source files are in src
+        "./frontend/**/*.{js,ts,jsx,tsx}",
+        "./**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
