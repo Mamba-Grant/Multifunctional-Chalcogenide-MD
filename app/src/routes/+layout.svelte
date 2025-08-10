cc865a78d0fbd8344be1dd5c7fdc5a2d06a39aa8
Grant Saggars <mumbo1002@gmail.com>
2025-08-10 17:46:42 -0500

    Added vercel speed insights

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
     let { children } = $props();
     import "../app.css";
+    injectSpeedInsights();
 </script>
 
 {@render children()}
